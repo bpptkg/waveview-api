ddbcb96de5b158389ab25787e1dbeb975e12b8a5
Indra Rudianto <indrarudianto.official@gmail.com>
2024-07-05 09:51:39 +0700

    chore: Add VolcanoMedia model for managing volcano media files

--- waveview/volcano/models.py
+++ waveview/volcano/models.py
@@ -2,9 +2,10 @@ import uuid
 
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
+
+from waveview.utils.media import MediaPath, MediaType
 
 
 class Volcano(models.Model):
@@ -18,11 +19,38 @@ class Volcano(models.Model):
     longitude = models.FloatField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     class Meta:
         verbose_name = _("volcano")
         verbose_name_plural = _("volcanoes")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class VolcanoMedia(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    volcano = models.ForeignKey(
+        Volcano,
+        on_delete=models.CASCADE,
+        related_name="media",
+        related_query_name="media",
+    )
+    file = models.FileField(upload_to=MediaPath("volcano-media"))
+    name = models.CharField(max_length=200, null=True, blank=True)
+    size = models.PositiveBigIntegerField()
+    media_type = models.CharField(max_length=50, choices=MediaType.choices)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = _("volcano media")
+        verbose_name_plural = _("volcano media")
 
     def __str__(self) -> str:
         return self.name

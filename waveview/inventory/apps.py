524a0e16c79d1c82e1b8c0f3e131e640f64ef354
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-02 20:50:44 +0700

    refactor: Add signal handlers for creating and dropping TimescaleDB tables for Channel model

--- waveview/inventory/apps.py
+++ waveview/inventory/apps.py
@@ -5,3 +5,6 @@ class InventoryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "waveview.inventory"
     verbose_name = "Inventory"
+
+    def ready(self) -> None:
+        import waveview.inventory.signals  # noqa: F401
---
+++ waveview/inventory/signals.py
@@ -0,0 +1,25 @@
+from typing import Any, Dict
+
+from django.db import connection
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
+from waveview.inventory.db.schema import TimescaleSchemaEditor
+from waveview.inventory.models.channel import Channel
+
+
+@receiver(post_save, sender=Channel)
+def channel_post_save(sender: Any, instance: Channel, **kwargs: Dict[str, Any]) -> None:
+    schema = TimescaleSchemaEditor(connection, atomic=True)
+    table = instance.get_datastream_id()
+    schema.create_table(table)
+    schema.create_hypertable(table)
+
+
+@receiver(post_delete, sender=Channel)
+def channel_post_delete(
+    sender: Any, instance: Channel, **kwargs: Dict[str, Any]
+) -> None:
+    schema = TimescaleSchemaEditor(connection, atomic=True)
+    table = instance.get_datastream_id()
+    schema.drop_table(table)

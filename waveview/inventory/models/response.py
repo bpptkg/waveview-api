c7ddea4e3b0593a0b683f25580b3cab5da71fc30
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-21 10:31:36 +0700

    chore: Update response model to use XML files for reading channel response temporarily

---
+++ waveview/inventory/models/response.py
@@ -0,0 +1,6 @@
+"""
+For now reading channel response is done through Inventory.file XML files.
+
+This is a temporary solution until the database is updated to include the
+channel response schema.
+"""

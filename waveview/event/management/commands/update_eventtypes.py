80142b4204ccc48bf4fc3e9a89a37f3cef8251ce
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-29 13:49:03 +0700

    feat: Update event types from JSON config file

---
+++ waveview/event/management/commands/update_eventtypes.py
@@ -0,0 +1,72 @@
+import json
+from typing import Any, TypedDict
+
+from django.core.management.base import BaseCommand, CommandParser
+from django.db import transaction
+
+from waveview.event.models import EventType
+from waveview.organization.models import Organization
+
+
+class EventTypeDict(TypedDict):
+    code: str
+    name: str
+    color: str
+    color_light: str
+    color_dark: str
+
+
+class Command(BaseCommand):
+    help = "Update event types from JSON config file."
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "path",
+            type=str,
+            help="Path to the JSON file containing list of event types.",
+        )
+        parser.add_argument(
+            "organization_id",
+            type=str,
+            help="Organization ID to update event types for.",
+        )
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        path = options["path"]
+        with open(path) as f:
+            event_types: list[EventTypeDict] = json.load(f)
+
+        organization_id = options["organization_id"]
+        try:
+            organization = Organization.objects.get(id=organization_id)
+        except Organization.DoesNotExist:
+            self.stdout.write(self.style.ERROR("Organization not found."))
+            return
+
+        with transaction.atomic():
+            for item in event_types:
+                code = item["code"]
+                name = item["name"]
+                color = item["color"]
+                color_light = item["color_light"]
+                color_dark = item["color_dark"]
+
+                event_type, created = EventType.objects.update_or_create(
+                    organization=organization,
+                    code=code,
+                    defaults={
+                        "name": name,
+                        "color": color,
+                        "color_light": color_light,
+                        "color_dark": color_dark,
+                    },
+                )
+
+                if created:
+                    self.stdout.write(
+                        self.style.SUCCESS(f"Created event type {event_type}")
+                    )
+                else:
+                    self.stdout.write(
+                        self.style.SUCCESS(f"Updated event type {event_type}")
+                    )

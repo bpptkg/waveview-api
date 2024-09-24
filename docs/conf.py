4e0c13e27c705c867c4e29802785ebff89ea8432
Indra Rudianto <indrarudianto.official@gmail.com>
2024-09-24 23:13:52 +0700

    Update Sphinx and sphinx-rtd-theme packages

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+project = "WaveView"
+copyright = "(c) 2024, WaveView Developers"
+author = "WaveView Developers"
+
+release = "0.1.0"
+
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.mathjax",
+]
+
+autosectionlabel_prefix_document = True
+
+templates_path = ["_templates"]
+
+source_suffix = ".rst"
+
+master_doc = "index"
+
+language = "en"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+pygments_style = "sphinx"
+
+html_theme = "sphinx_rtd_theme"
+
+html_favicon = "waveview.svg"
---
+++ docs/index.rst
@@ -0,0 +1,7 @@
+============
+WaveView API
+============
+
+WaveView is a web app that provide realtime seismic waveform visualization,
+picking tools, event management, and various signal analysis particularly for
+Volcano monitoring.

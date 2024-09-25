f330ffbc9e38d0e855ad84e654734544b41f6f06
Indra Rudianto <indrarudianto.official@gmail.com>
2024-09-25 17:35:31 +0700

    Refactor MediaPath class in media.py to include an optional prefix parameter

--- waveview/utils/media.py
+++ waveview/utils/media.py
@@ -2,20 +2,24 @@ import os
 from uuid import uuid4
 
 from django.db import models
+from django.db.models import Model
 from django.utils import timezone
 from django.utils.deconstruct import deconstructible
 
 
 @deconstructible
 class MediaPath(object):
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, prefix: bool = True) -> None:
         self.path = path
+        self.prefix = prefix
 
-    def __call__(self, instance: object, filename: str) -> str:
+    def __call__(self, instance: Model, filename: str) -> str:
         ext = filename.split(".")[-1]
         filename = "{}.{}".format(uuid4(), ext)
         today = timezone.now().date()
         path = today.strftime(self.path)
+        if self.prefix:
+            return os.path.join(path, str(instance.id), filename)
         return os.path.join(path, filename)
 
 

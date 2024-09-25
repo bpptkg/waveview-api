392d4748c2f9fbd307b77c07ecdc80f32b9d6891
Indra Rudianto <indrarudianto.official@gmail.com>
2024-09-25 18:29:45 +0700

    Refactor BulletinObserver in observer.py to include delay and retry logic for creating, updating, and deleting bulletins

--- waveview/contrib/bma/bulletin/observer.py
+++ waveview/contrib/bma/bulletin/observer.py
@@ -5,15 +5,26 @@ from waveview.contrib.bma.bulletin.payload import BulletinPayloadBuilder
 from waveview.contrib.bma.bulletin.types import BulletinData
 from waveview.event.models import Event
 from waveview.event.observers import EventObserver
+from waveview.utils.delay import delay
 from waveview.utils.retry import retry
 
 logger = logging.getLogger(__name__)
 
 
 class BulletinObserver(EventObserver):
+    """
+    This observer is responsible for creating, updating and deleting bulletins
+    in the BMA system.
+
+    When the observer receives an event, it should wait for certain amount of
+    time before performing the operation to wait for the data, e.g magnitude,
+    amplitude, and location to be available in the database.
+    """
+
     name = "bma.bulletin"
 
-    @retry()
+    @delay(60)
+    @retry(initial_delay=5)
     def create(self, event_id: str, data: dict) -> None:
         event = Event.objects.get(id=event_id)
         payload = BulletinPayloadBuilder(event).build()
@@ -21,7 +32,8 @@ class BulletinObserver(EventObserver):
         client = BulletinClient(conf.server_url, conf.token)
         client.create(payload)
 
-    @retry()
+    @delay(60)
+    @retry(initial_delay=5)
     def update(self, event_id: str, data: dict) -> None:
         event = Event.objects.get(id=event_id)
         payload = BulletinPayloadBuilder(event).build()
@@ -29,7 +41,8 @@ class BulletinObserver(EventObserver):
         client = BulletinClient(conf.server_url, conf.token)
         client.update(event_id, payload)
 
-    @retry()
+    @delay(60)
+    @retry(initial_delay=5)
     def delete(self, event_id: str, data: dict) -> None:
         conf = BulletinData.from_dict(data)
         client = BulletinClient(conf.server_url, conf.token)

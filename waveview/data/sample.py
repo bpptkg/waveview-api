5bb746e686fdaf3d0913c72091769023428deb9e
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-19 12:55:44 +0700

    Add get_sample_waveform to return an ObsPy Stream object

--- waveview/data/sample.py
+++ waveview/data/sample.py
@@ -1,7 +1,16 @@
 from pathlib import Path
 
+from obspy import Stream, read
+
 SAMPLE_DIR = Path(__file__).resolve().parent
 
 
 def get_sample_file_path(filename: str) -> Path:
     return SAMPLE_DIR / filename
+
+
+def get_sample_waveform() -> Stream:
+    """
+    Returns an ObsPy Stream object from the sample.mseed file.
+    """
+    return read(get_sample_file_path("sample.mseed"))

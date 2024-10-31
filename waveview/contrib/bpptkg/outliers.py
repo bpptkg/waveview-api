1b30946288e44d184c3626a90726b114e8284e96
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-31 14:12:53 +0700

    Rename remove_outliers function to clean_outliers and add windowed outlier removal function

--- waveview/contrib/bpptkg/outliers.py
+++ waveview/contrib/bpptkg/outliers.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def remove_outliers(data: np.ndarray) -> np.ndarray:
+def clean_outliers(data: np.ndarray) -> np.ndarray:
     if len(data) == 0:
         return data
     q1 = np.quantile(data, 0.1)
@@ -13,3 +13,14 @@ def remove_outliers(data: np.ndarray) -> np.ndarray:
     upper_bound = q3 + threshold
     data[(data <= lower_bound) | (data >= upper_bound)] = 0
     return data
+
+
+def remove_outliers(data: np.ndarray, window_size: int = 500) -> np.ndarray:
+    result = np.zeros_like(data)
+    for i in range(len(data) - window_size + 1):
+        window = data[i : i + window_size]
+        cleaned_window = clean_outliers(window)
+        result[i + window_size // 2] = cleaned_window[
+            window_size // 2
+        ]  # Assign the middle value.
+    return result

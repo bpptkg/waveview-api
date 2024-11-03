06d26fa5df5ee6e5cb01f1bd1a44c09bfd4c0ad6
Indra Rudianto <indrarudianto.official@gmail.com>
2024-11-03 12:36:58 +0700

    Fix run asgi with daphne

--- waveview/asgi.py
+++ waveview/asgi.py
@@ -1,17 +1,22 @@
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "waveview.settings")
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+asgi_app = get_asgi_application()
+
 from waveview.routing import websocket_urlpatterns
 from waveview.websocket.middleware import JWTAuthMiddleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "waveview.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": asgi_app,
         "websocket": AllowedHostsOriginValidator(
             JWTAuthMiddleware(URLRouter(websocket_urlpatterns))
         ),

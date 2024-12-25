d999e28028c80ef6b98dd9009ebdbd4bf2153af3
Indra Rudianto <indrarudianto.official@gmail.com>
2024-12-25 21:13:43 +0700

    Enhance ASGI setup by adding comments for clarity and organization

--- waveview/asgi.py
+++ waveview/asgi.py
@@ -1,18 +1,22 @@
 import os
+
 import django
 
+# Setup Django ASGI application.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "waveview.settings")
 django.setup()
 
+# Import ASGI modules.
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+# Get the Django ASGI application.
 asgi_app = get_asgi_application()
 
+# Import the websocket URL patterns.
 from waveview.routing import websocket_urlpatterns
 from waveview.websocket.middleware import JWTAuthMiddleware
-
 
 application = ProtocolTypeRouter(
     {

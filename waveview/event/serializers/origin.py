0492322c8253c008ce3ccbc226a1c024dc64677a
Indra Rudianto <indrarudianto.official@gmail.com>
2025-02-14 22:48:08 +0700

    fix: Enhance help text in OriginSerializer and OriginPayloadSerializer for clarity and specify units for latitude, longitude, depth, and time format

--- waveview/event/serializers/origin.py
+++ waveview/event/serializers/origin.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
+from waveview.event.header import EvaluationMode, EvaluationStatus
 from waveview.event.models import Origin
 
 
@@ -8,43 +9,61 @@ class OriginSerializer(serializers.Serializer):
     id = serializers.UUIDField(help_text=_("Origin ID."))
     event_id = serializers.UUIDField(help_text=_("Event ID."))
     time = serializers.DateTimeField(help_text=_("Origin time."))
-    latitude = serializers.FloatField(help_text=_("Origin latitude."), allow_null=True)
+    latitude = serializers.FloatField(
+        help_text=_("Origin latitude in degree."), allow_null=True
+    )
     longitude = serializers.FloatField(
-        help_text=_("Origin longitude."), allow_null=True
+        help_text=_("Origin longitude in degree."), allow_null=True
     )
-    depth = serializers.FloatField(help_text=_("Origin depth."), allow_null=True)
-    method = serializers.CharField(help_text=_("Origin method."), allow_null=True)
+    depth = serializers.FloatField(help_text=_("Origin depth in km."), allow_null=True)
+    method = serializers.CharField(help_text=_("Origin method name."), allow_null=True)
     earth_model = serializers.CharField(
-        help_text=_("Origin earth model."), allow_null=True
+        help_text=_("Origin earth model name."), allow_null=True
     )
-    evaluation_mode = serializers.CharField(
-        help_text=_("Origin evaluation mode."), allow_null=True
+    evaluation_mode = serializers.ChoiceField(
+        help_text=_("Origin evaluation mode."),
+        allow_null=True,
+        choices=EvaluationMode.choices,
     )
-    evaluation_status = serializers.CharField(
-        help_text=_("Origin evaluation status."), allow_null=True
+    evaluation_status = serializers.ChoiceField(
+        help_text=_("Origin evaluation status."),
+        allow_null=True,
+        choices=EvaluationStatus.choices,
     )
     created_at = serializers.DateTimeField(help_text=_("Origin created at."))
     updated_at = serializers.DateTimeField(help_text=_("Origin updated at."))
     author_id = serializers.UUIDField(help_text=_("Author ID."))
-    is_preferred = serializers.BooleanField(help_text=_("Is preferred origin."))
+    is_preferred = serializers.BooleanField(
+        help_text=_("True if this origin is preferred.")
+    )
 
 
 class OriginPayloadSerializer(serializers.Serializer):
-    time = serializers.DateTimeField(help_text=_("Origin time."))
-    latitude = serializers.FloatField(help_text=_("Origin latitude."))
-    longitude = serializers.FloatField(help_text=_("Origin longitude."))
-    depth = serializers.FloatField(help_text=_("Origin depth."))
+    time = serializers.DateTimeField(help_text=_("Origin time in ISO 8601 format."))
+    latitude = serializers.FloatField(help_text=_("Origin latitude in degree."))
+    longitude = serializers.FloatField(help_text=_("Origin longitude in degree."))
+    depth = serializers.FloatField(help_text=_("Origin depth in km."))
     method = serializers.CharField(
-        help_text=_("Origin method."), allow_null=True, allow_blank=True
+        help_text=_(
+            "Method name used to calculate the origin, e.g. btbb, hypo71, etc."
+        ),
+        allow_null=True,
+        allow_blank=True,
     )
     earth_model = serializers.CharField(
-        help_text=_("Origin earth model."), allow_null=True, allow_blank=True
+        help_text=_("Origin earth model name."), allow_null=True, allow_blank=True
     )
-    evaluation_mode = serializers.CharField(
-        help_text=_("Origin evaluation mode."), allow_null=True, allow_blank=True
+    evaluation_mode = serializers.ChoiceField(
+        choices=EvaluationMode.choices,
+        help_text=_("Origin evaluation mode."),
+        allow_null=True,
+        allow_blank=True,
     )
-    evaluation_status = serializers.CharField(
-        help_text=_("Origin evaluation status."), allow_null=True, allow_blank=True
+    evaluation_status = serializers.ChoiceField(
+        choices=EvaluationStatus.choices,
+        help_text=_("Origin evaluation status."),
+        allow_null=True,
+        allow_blank=True,
     )
 
     def create(self, validated_data: dict) -> Origin:

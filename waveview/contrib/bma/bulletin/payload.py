72a10697a49bcf13005bd161e630b92962ed12b7
Indra Rudianto <indrarudianto.official@gmail.com>
2025-04-19 07:58:13 +0700

    fix: Handle None case for amplitude in BulletinPayloadBuilder

--- waveview/contrib/bma/bulletin/payload.py
+++ waveview/contrib/bma/bulletin/payload.py
@@ -21,6 +21,8 @@ class BulletinPayloadBuilder:
         ).first()
         if amplitude is None:
             return None
+        if amplitude.amplitude is None:
+            return None
         return f"{amplitude.amplitude:.2f}{amplitude.unit}"
 
     def _get_ml_deles(self) -> float | None:

0aa5799d746f877a7f8918abbc9b337eafd07e83
Indra Rudianto <indrarudianto.official@gmail.com>
2025-05-01 13:56:15 +0700

    refactor: Simplify BPPTKGMagnitudeEstimator by integrating amplitude calculations and removing unused methods

--- waveview/contrib/bpptkg/magnitude.py
+++ waveview/contrib/bpptkg/magnitude.py
@@ -1,19 +1,11 @@
 import logging
 from dataclasses import dataclass
-from datetime import timedelta
 
 import numpy as np
 from django.db import connection, transaction
-from obspy import Stream
 
-from waveview.contrib.bpptkg.outliers import remove_outliers
-from waveview.contrib.bpptkg.response import remove_instrument_response
-from waveview.event.header import (
-    AmplitudeCategory,
-    AmplitudeUnit,
-    EvaluationMode,
-    EvaluationStatus,
-)
+from waveview.contrib.bpptkg.amplitude import BPPTKGAmplitudeCalculator
+from waveview.event.header import AmplitudeUnit, EvaluationMode, EvaluationStatus
 from waveview.event.models import (
     Amplitude,
     Event,
@@ -23,7 +15,7 @@ from waveview.event.models import (
 )
 from waveview.event.observers import EventObserver
 from waveview.inventory.datastream import DataStream
-from waveview.inventory.models import Channel, Inventory
+from waveview.inventory.models import Channel
 
 logger = logging.getLogger(__name__)
 
@@ -68,83 +60,48 @@ class MagnitudeObserverData:
         )
 
 
-def calc_bpptkg_ml(amplitude: float) -> float:
-    """
-    Compute BPPTKG Richter local magnitude scale (ML) from amplitude.
-
-    Note that amplitude is in meter, but in calculation, it uses micro-meter.
-    Calibration function log10(A0) for BPPTKG seismic network is -1.4.
-
-    Richter magnitude scale is computed using the following equation:
-
-        ml = log10(amplitude) - log10(A0)
-
-    where log10(A0) equal to -1.4 and ml is Richter local magnitude scale.
-    """
-    ampl = amplitude * 1e6  # Convert to micro-meter.
-    return np.log10(ampl) + 1.4
-
-
 @dataclass
 class MagnitudeEstimatorData:
     event: Event
     data: MagnitudeObserverData
 
 
-class MagnitudeEstimator:
+class BPPTKGMagnitudeEstimator:
     method = "bpptkg"
 
     def __init__(self) -> None:
         self.datastream = DataStream(connection)
         self.preferred_map: dict[str, bool] = {}
 
-    def get_inventory(self, organization_id: str) -> Inventory:
-        inventory = Inventory.objects.get(organization_id=organization_id)
-        return inventory
-
-    def get_amax(self, data: np.ndarray) -> float | None:
+    def calc_bpptkg_ml(self, amplitude: float) -> float:
         """
-        Get Amax (peak-to-peak/2) value from stream in m.
+        Compute BPPTKG Richter local magnitude scale (ML) from amplitude.
+
+        Note that amplitude is in meter, but in calculation, it uses micro-meter.
+        Calibration function log10(A0) for BPPTKG seismic network is -1.4.
+
+        Richter magnitude scale is computed using the following equation:
+
+            ml = log10(amplitude) - log10(A0)
+
+        where log10(A0) equal to -1.4 and ml is Richter local magnitude scale.
         """
-        if len(data) == 0:
-            return None
-
-        minval = np.nanmin(data)
-        maxval = np.nanmax(data)
-        amplitude = (maxval - minval) / 2
-        if np.isnan(amplitude):
-            return None
-        return amplitude
-
-    def remove_response(self, inventory: Inventory, st: Stream) -> Stream:
-        return remove_instrument_response(inventory, st)
+        return np.log10(amplitude) + 1.4
 
     @transaction.atomic
-    def calc_magnitude(
+    def calc(
         self, data: MagnitudeEstimatorData, use_outlier_filter: bool = False
     ) -> None:
         event = data.event
-        organization_id = event.catalog.volcano.organization.id
-        inventory = self.get_inventory(organization_id)
+        logger.info(f"Calculating BPPTKG ML magnitude for event {event.id}...")
 
+        organization_id = event.catalog.volcano.organization.id
         options = data.data
         author_id = event.author.id
         is_preferred = options.is_preferred
         preferred_stream_id = options.preferred_stream_id
         analogs = options.analogs
         stream_ids = options.stream_ids
-
-        logger.info(f"Calculating BPPTKG ML magnitude for event {event.id}...")
-
-        if use_outlier_filter:
-            buffer = 5  # Buffer in seconds.
-            starttime = event.time - timedelta(seconds=buffer)
-            endtime = event.time + timedelta(seconds=event.duration + buffer)
-        else:
-            buffer = 0
-            starttime = event.time
-            endtime = event.time + timedelta(seconds=event.duration)
-
         magnitude_type = "ML"
 
         magnitude, _ = Magnitude.objects.get_or_create(
@@ -165,46 +122,40 @@ class MagnitudeEstimator:
         stations: set[str] = set()
         amplitude_map: dict[str, Amplitude] = {}
 
-        for channel in Channel.objects.all():
-            st = self.datastream.get_waveform(channel.id, starttime, endtime)
+        amplitude_calculator = BPPTKGAmplitudeCalculator()
 
-            try:
-                st = self.remove_response(inventory, st)
-            except Exception as e:
-                logger.error(
-                    f"Failed to remove response for channel {channel.stream_id}: {e}"
-                )
-                continue
+        for channel in Channel.objects.filter(
+            station__network__inventory__organization_id=organization_id
+        ).all():
+            channel_id = channel.id
+            sa = amplitude_calculator.calc(
+                event.time,
+                event.duration,
+                channel_id,
+                organization_id,
+                use_outlier_filter=use_outlier_filter,
+            )
 
-            if len(st) == 0:
-                amax = None
-                ml = None
-            else:
-                data = remove_outliers(st[0].data) if use_outlier_filter else st[0].data
-                amax = self.get_amax(data)
-                ml = calc_bpptkg_ml(amax)
+            amax = sa.amplitude
+            ml = self.calc_bpptkg_ml(amax) if amax is not None else None
 
             if channel.contains_stream_id(stream_ids) and ml is not None:
                 magnitude_values.append(ml)
                 stations.add(channel.station.code)
-
-            uamax = (
-                amax * 1e6 if amax is not None else None
-            )  # Convert to µm if amax exists
 
             amplitude, _ = Amplitude.objects.update_or_create(
                 event=event,
                 waveform=channel,
                 method=self.method,
                 defaults={
-                    "amplitude": uamax,
+                    "amplitude": amax,
                     "type": "Amax",
-                    "category": AmplitudeCategory.DURATION,
-                    "time": event.time,
-                    "begin": buffer,
-                    "end": event.duration + buffer,
+                    "category": sa.category,
+                    "time": sa.time,
+                    "begin": sa.begin,
+                    "end": sa.end,
                     "snr": 0,
-                    "unit": AmplitudeUnit.UM.label,
+                    "unit": sa.unit,
                     "evaluation_mode": EvaluationMode.AUTOMATIC,
                     "author_id": author_id,
                     "is_preferred": channel.matches_stream_id(preferred_stream_id),
@@ -260,10 +211,10 @@ class MagnitudeEstimator:
                 defaults={
                     "amplitude": value,
                     "type": "Amax",
-                    "category": AmplitudeCategory.DURATION,
-                    "time": event.time,
-                    "begin": buffer,
-                    "end": event.duration + buffer,
+                    "category": ampl.category,
+                    "time": ampl.time,
+                    "begin": ampl.begin,
+                    "end": ampl.end,
                     "snr": 0,
                     "unit": AmplitudeUnit.MM.label,
                     "evaluation_mode": EvaluationMode.AUTOMATIC,
@@ -273,7 +224,7 @@ class MagnitudeEstimator:
                 },
             )
 
-        logger.info(f"BPPTKG ML magnitude calculation for event {event.id} is done.")
+        logger.info(f"BPPTKG ML magnitude for event {event.id} calculated.")
 
 
 class MagnitudeObserver(EventObserver):
@@ -287,8 +238,8 @@ class MagnitudeObserver(EventObserver):
         self, event_id: str, data: dict, use_outlier_filter: bool = False
     ) -> None:
         event = Event.objects.get(id=event_id)
-        estimator = MagnitudeEstimator()
-        estimator.calc_magnitude(
+        estimator = BPPTKGMagnitudeEstimator()
+        estimator.calc(
             MagnitudeEstimatorData(
                 event=event, data=MagnitudeObserverData.from_dict(data)
             ),

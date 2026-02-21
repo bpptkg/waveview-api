0462c403b484e898d82c0915895fe061a4fd80db
Indra Rudianto <indrarudianto.official@gmail.com>
2026-02-21 10:27:31 +0700

    feat: remove update_inventory from signals in favor of command line

--- waveview/inventory/signals.py
+++ waveview/inventory/signals.py
@@ -5,9 +5,7 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from waveview.inventory.db.schema import TimescaleSchemaEditor
-from waveview.inventory.models import InventoryFile
 from waveview.inventory.models.channel import Channel
-from waveview.tasks.update_inventory import update_inventory
 
 
 @receiver(post_save, sender=Channel)
@@ -26,11 +24,3 @@ def channel_post_delete(
     schema = TimescaleSchemaEditor(connection, atomic=True)
     table = instance.get_datastream_id()
     schema.drop_table(table)
-
-
-@receiver(post_save, sender=InventoryFile)
-def inventory_post_save(
-    sender: Any, instance: InventoryFile, **kwargs: Dict[str, Any]
-) -> None:
-    inventory_id = str(instance.id)
-    update_inventory.delay(inventory_id)
